3d04cb276dc9fd3d7c7112cbc186b9c2287c5383
Macohi <sinconsistencia@gmail.com>
2026-02-09 18:15:07 -0600

    should be and

--- util/ControlClass.hx
+++ util/ControlClass.hx
@@ -7,13 +7,13 @@ class ControlClass
 	public var keybinds:Map<String, Array<String>> = [];
 
 	public function pressed(keybind:String):Bool
-		return keybinds.exists(keybind) || FlxG.keys.anyPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
+		return keybinds.exists(keybind) && FlxG.keys.anyPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
 	public function justPressed(keybind:String):Bool
-		return keybinds.exists(keybind) || FlxG.keys.anyJustPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
+		return keybinds.exists(keybind) && FlxG.keys.anyJustPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
 	public function justReleased(keybind:String):Bool
-		return keybinds.exists(keybind) || FlxG.keys.anyJustReleased(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
+		return keybinds.exists(keybind) && FlxG.keys.anyJustReleased(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
 	public function new() {}
 }
