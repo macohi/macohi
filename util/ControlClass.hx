57500ca61bf295112ecbde5b98ed2c6b00d86025
Macohi <sinconsistencia@gmail.com>
2026-02-09 18:04:40 -0600

    rename methods

--- util/ControlClass.hx
+++ util/ControlClass.hx
@@ -6,13 +6,13 @@ class ControlClass
 {
 	public var keybinds:Map<String, Array<String>> = [];
 
-	public function checkForKeybindPressed(keybind:String):Bool
+	public function pressed(keybind:String):Bool
 		return keybinds.exists(keybind) || FlxG.keys.anyPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
-	public function checkForKeybindJustPressed(keybind:String):Bool
+	public function justPressed(keybind:String):Bool
 		return keybinds.exists(keybind) || FlxG.keys.anyJustPressed(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
-	public function checkForKeybindJustReleased(keybind:String):Bool
+	public function justReleased(keybind:String):Bool
 		return keybinds.exists(keybind) || FlxG.keys.anyJustReleased(ArrayUtil.convertStringToFlxKey(keybinds.get(keybind)));
 
 	public function new() {}
