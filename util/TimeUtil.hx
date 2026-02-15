f14c3da7bd9d4fb8fc43434eddee68a633392e73
Macohi <sinconsistencia@gmail.com>
2026-02-15 14:08:36 -0600

    time util

---
+++ util/TimeUtil.hx
@@ -0,0 +1,12 @@
+package macohi.util;
+
+import flixel.math.FlxMath;
+
+class TimeUtil
+{
+	public static function round(time:Float):Float
+		return FlxMath.roundDecimal(time, 2);
+
+	public static function convert_ms_to_s(milliseconds:Float):Float
+		return milliseconds / 1000;
+}
