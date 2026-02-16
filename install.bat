2880538a24b71896fa3f73fc868a4a86047ae49c
Macohi <sinconsistencia@gmail.com>
2026-02-16 12:53:15 -0600

    hxcpp

--- install.bat
+++ install.bat
@@ -1,10 +1,15 @@
 : Do you really want this a global thing? Then disable this.
 haxelib newrepo
 
+haxelib install hxcpp
+haxelib install hxcpp-debug-server
+
 haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install flixel-animate
+
 haxelib install openfl
 haxelib install lime
+
 haxelib install newgrounds
