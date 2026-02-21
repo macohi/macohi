91815d959c27e3f67045d73a6a3069619ac34c81
Macohi <sinconsistencia@gmail.com>
2026-02-20 21:45:59 -0600

    randomDirection function

--- util/Direction.hx
+++ util/Direction.hx
@@ -1,9 +1,14 @@
 package macohi.util;
 
+import flixel.FlxG;
+
 enum abstract Direction(Int) from Int to Int
 {
 	var LEFT = 0;
 	var DOWN = 1;
 	var UP = 2;
 	var RIGHT = 3;
+
+	public static function randomDirection():Direction
+		return FlxG.random.int(0, 3);
 }
