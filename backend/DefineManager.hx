ea6a6c87d2ad9fe73c0268206e56c8115c9a9b41
Macohi <sinconsistencia@gmail.com>
2026-02-21 09:36:27 -0600

    define manager

---
+++ backend/DefineManager.hx
@@ -0,0 +1,37 @@
+package macohi.backend;
+
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+import haxe.macro.Expr.ExprOf;
+import haxe.macro.Expr;
+
+class DefineManager
+{
+	public static var definesAndTheirFunctions:Map<String, Dynamic->Void> = [];
+
+	public static macro function getDefine(define:String)
+		return macro $v{Context.definedValue(key)};
+
+	public static function isDefineDefined(define:String):Bool
+		return getDefine(define) != null;
+
+	public static function doesDefineHaveValue(define:String):Bool
+		return isDefineDefined() && getDefine(define) != '1';
+
+	public static function areAnyDefinesDefined(defines:Array<String>):Bool
+	{
+		for (define in defines)
+			if (isDefineDefined(define))
+				return true;
+
+		return false;
+	}
+
+	public static function runIfDefineDefined(define:String, toRun:Dynamic->Void):Bool
+		if (isDefineDefined(define) && toRun != null)
+			toRun(getDefine(define));
+
+	public static function parseDefinesAndTheirFunctions()
+		for (define => toRun in definesAndTheirFunctions)
+			runIfDefineDefined(define, toRun);
+}
