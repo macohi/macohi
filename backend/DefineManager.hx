370f3ac2a38010d43e6404936bfd929072837384
Macohi <sinconsistencia@gmail.com>
2026-02-21 09:41:33 -0600

    macros are annoying as shit.

--- backend/DefineManager.hx
+++ backend/DefineManager.hx
@@ -3,22 +3,21 @@ package macohi.backend;
 import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.Expr.ExprOf;
-import haxe.macro.Expr;
 
 class DefineManager
 {
-	public static var definesAndTheirFunctions:Map<String, Array<Dynamic->Void>> = [];
+	public static var definesAndTheirFunctions:Map<ExprOf<String>, Array<Dynamic->Void>> = [];
 
-	public static macro function getDefine(define:String)
+	public static macro function getDefine(define:ExprOf<String>)
 		return macro $v{Context.definedValue(key)};
 
-	public static function isDefineDefined(define:String):Bool
+	public static function isDefineDefined(define:ExprOf<String>):Bool
 		return getDefine(define) != null;
 
-	public static function doesDefineHaveValue(define:String):Bool
+	public static function doesDefineHaveValue(define:ExprOf<String>):Bool
 		return isDefineDefined() && getDefine(define) != '1';
 
-	public static function areAnyDefinesDefined(defines:Array<String>):Bool
+	public static function areAnyDefinesDefined(defines:Array<ExprOf<String>>):Bool
 	{
 		for (define in defines)
 			if (isDefineDefined(define))
@@ -27,11 +26,11 @@ class DefineManager
 		return false;
 	}
 
-	public static function runIfDefineDefined(define:String, toRun:Dynamic->Void)
+	public static function runIfDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
 		if (isDefineDefined(define) && toRun != null)
 			toRun(getDefine(define));
 
-	public static function runIfNotDefineDefined(define:String, toRun:Dynamic->Void)
+	public static function runIfNotDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
 		if (!isDefineDefined(define) && toRun != null)
 			toRun(getDefine(define));
 
